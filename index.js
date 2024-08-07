ee7fd798473aacaf64de76fd0db57c9989007bf8
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-07 23:00:46 +0300

    add navigation , mobile header

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-links a');
+    const mobileNavLinks = document.querySelectorAll('.nav-links-mobile a');
+    const burger = document.querySelector('.burger');
+    sections[0].classList.add('active');
+   const mobileDrawer = document.getElementById('mobile-drawer');
+
+
+
+    const handleContentChange = (link) => {
+     
+            
+        sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            
+            sections.forEach((section) => {
+                if (section.getAttribute('id') === link.getAttribute('href').replace('#', '')) {
+                    section.classList.add('active');
+                }
+            })
+    }
+
+
+
+
+    navLinks.forEach((link) => {
+
+
         
-   document.addEventListener('mouseover', (e) => {
-    if(e.target.classList.contains('menu-button')) {
-        e.target.style.color = 'red';
-        e.target.style.cursor = 'pointer';
+link.addEventListener('click', (e) => {
+            e.preventDefault();
+             handleContentChange(link);
+        
+        });
     
-    }
-   })
+    
+    
+    
+    
+    });
 
-   document.addEventListener('mouseout', (e) => {
-    if(e.target.classList.contains('menu-button')) {
-        e.target.style.color = '';
-    }
-});
+    mobileNavLinks.forEach((link) => {
 
 
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+           handleContentChange(link);
+         mobileDrawer.classList.remove('active');
       
-});
+    
+    
+    
+        });
+       
+
+
+    });
+
+    burger.addEventListener('click',() => {
+            mobileDrawer.classList.toggle('active');
+    });
+
+    
+
+
+
+}); 
