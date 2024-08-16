02623340ed52e16f80323db1f9097c49866f53a3
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-17 02:17:56 +0300

    add event listeners for  style for each MenuItem

--- my-porfolio/src/components/menuItem/MenuItem.jsx
+++ my-porfolio/src/components/menuItem/MenuItem.jsx
@@ -1,9 +1,12 @@
 import { motion } from 'framer-motion';
 import React from 'react';
+import { useEffect, useRef } from 'react';
 import './menuItem.css';
 
 
 const MenuItem = ({ string, i }) => {
+
+    const itemRef = useRef();
 
     const variants = {
         open: {
@@ -22,11 +25,41 @@ const MenuItem = ({ string, i }) => {
         }
     }
 
-    const colors = ['#FF008C', '#D309E1', '#9C1AFF', '#7700FF', '#4400FF'];
+    useEffect(() => {
+        const item = itemRef.current;
+        const handleMouseEnter = (e) => {
+            const rect = item.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            item.style.setProperty("--x", `${x}px`);
+            item.style.setProperty("--y", `${y}px`);
+            item.classList.add('animate')
+        }
+
+        const handleMouseLeave = () => {
+            item.classList.remove('animate')
+        }
+
+        item.addEventListener('mouseenter', handleMouseEnter);
+        item.addEventListener('mouseleave', handleMouseLeave);
+
+        return () => {
+            item.removeEventListener('mouseenter', handleMouseEnter);
+            item.removeEventListener('mouseleave', handleMouseLeave);
+        }
+    }, [])
+
+
 
     return (
         <motion.li layout variants={variants}>
-            <motion.div layout className='menu-item' whileHover={{scale: 1.2}}>{string}</motion.div>
+            <motion.div layout className='menu-item'
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.8 }}
+                ref={itemRef}>
+                {string}
+                <div className="item-background"></div>
+            </motion.div>
 
         </motion.li>
     )
