a7caa44f4df7125c8bde70caba4c9be68bfd982c
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-20 18:22:18 +0300

    add AboutMe component

---
+++ my-porfolio/src/components/aboutMe/AboutMe.jsx
@@ -0,0 +1,16 @@
+import {motion} from 'framer-motion'
+import './aboutMe.css'
+const AboutMe = () => {
+    
+
+
+    return (
+        <motion.div id='about' >
+            <motion.h1>
+                About Me
+            </motion.h1>
+        </motion.div>
+    )
+}
+
+export default AboutMe;
