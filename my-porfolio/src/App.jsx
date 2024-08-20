47f9fe15d308c83c20a6e3434daf60109d61d8f0
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-20 18:21:01 +0300

    scroll to top on component mount

--- my-porfolio/src/App.jsx
+++ my-porfolio/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar from './components/navBar/NavBar.jsx'
 import Introduction from './components/introduction/Introduction.jsx'
+import AboutMe from './components/aboutMe/AboutMe.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+  }, []);
+
   return (
     <>
-     <NavBar/> 
-      <Introduction/>
+      <NavBar />
+      <Introduction />
+      <AboutMe />
     </>
   )
 }
