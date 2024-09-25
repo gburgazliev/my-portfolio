fc22555b7a5252aa433480c04cc057fb9b976eff
gburgazliev <burgazlievgeorgi@gmail.com>
2024-09-25 22:10:22 +0300

    add scrollLeft Animation and style changes

--- my-porfolio/src/components/skills/Skills.jsx
+++ my-porfolio/src/components/skills/Skills.jsx
@@ -6,62 +6,54 @@ import Reveal from "../Reveal/Reveal";
 import './skills.css'
 
 const Skills = () => {
-
+    let skillCounter = 1;
     const technologies = [
-        { name: 'React', logo: <SiReact size='30px' style={{ color: 'lightblue' }} /> },
-        { name: 'JavaScript', logo: <TbBrandJavascript size='30px' fill="yellow" /> },
-        { name: 'HTML5', logo: <SiHtml5 size='30px' style={{ color: 'darkorange', }} /> },
-        { name: 'CSS', logo: <IoLogoCss3 size='35px' style={{ color: 'blue', }} /> },
-        { name: 'Chakra UI', logo: <SiChakraui size='30px' style={{ color: 'lightgreen', }} /> },
-        { name: 'Github', logo: <SiGithub size='30px' /> },
-        { name: 'Node.js', logo: <SiNodedotjs size='30px' fill="green" /> },
-        { name: 'Jest', logo: <SiJest fill="red" size='30px' /> },
-        { name: 'ESLint', logo: <SiEslint fill="lightblue" size='30px' /> },
-        { name: 'Tailwind CSS', logo: <SiTailwindcss size='30px' fill="lightblue" /> },
+        { name: 'React', logo: <SiReact size='40px' style={{ color: 'lightblue' }} /> },
+        { name: 'JavaScript', logo: <TbBrandJavascript size='40px' fill="yellow" /> },
+        { name: 'HTML5', logo: <SiHtml5 size='40px' style={{ color: 'darkorange', }} /> },
+        { name: 'CSS', logo: <IoLogoCss3 size='40px' style={{ color: 'blue', }} /> },
+        { name: 'Chakra UI', logo: <SiChakraui size='40px' style={{ color: 'lightgreen', }} /> },
+        { name: 'Github', logo: <SiGithub size='40px' /> },
+        { name: 'Node.js', logo: <SiNodedotjs size='40' fill="green" /> },
+        { name: 'Jest', logo: <SiJest fill="red" size='40px' /> },
+        { name: 'ESLint', logo: <SiEslint fill="lightblue" size='40px' /> },
+        { name: 'Tailwind CSS', logo: <SiTailwindcss size='40px' fill="lightblue" /> },
         { name: 'Daisy UI', logo: <SiDaisyui size='40px' color="lightblue" /> },
     ]
 
 
     return (
-         <Reveal>
-        <div id="skills">
-           
-
-            
-        <h2 style={{color: 'white', textAlign: 'center'}}>Technologies I have worked with</h2>
-            <div className="container">
-                <div className="technologies-container">
-                    {technologies.map((object) => {
-                        return <div style={{
-                            display: 'flex',
-
-                            fontSize: '1rem',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            overflow: 'hidden',
-
-
-                            padding: '5px',
-                            borderRadius: '13px',
-                         
-                        }}>
-                            <span>{object.logo}</span>   <span style={{ color: 'white', textAlign: 'center' }}>{object.name}</span>
-                        </div>
+        <Reveal>
+            <div id="skills">
 
 
 
-                    })}
+                <h2 style={{ color: 'white', textAlign: 'center' , fontSize: '1rem', }}>Technologies I have worked with</h2>
+                <div className="container">
+                    <div className="technologies-container">
+                        {technologies.map((object) => {
+                            const individualSkillClass = `item${skillCounter++}`
+                            
+                            return <div 
+                            className={'item' + ' ' + individualSkillClass} 
+                           >
+                                <span>{object.logo}</span>   <span style={{ color: 'white', textAlign: 'center' }}>{object.name}</span>
+                            </div>
+
+
+
+                        })}
+                    </div>
+
+
+
+
+
                 </div>
 
 
 
-
-
-            </div>
-
-
-
-        </div></Reveal>
+            </div></Reveal>
     )
 }
 
