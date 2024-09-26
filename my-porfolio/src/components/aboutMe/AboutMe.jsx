2e739e53ff8237332af514ec2e9cc8f188661da6
gburgazliev <burgazlievgeorgi@gmail.com>
2024-09-26 15:09:07 +0300

    update aboutme text

--- my-porfolio/src/components/aboutMe/AboutMe.jsx
+++ my-porfolio/src/components/aboutMe/AboutMe.jsx
@@ -14,11 +14,10 @@ const AboutMe = () => {
                     About Me
                 </motion.h1>
                 <motion.p layout>
-              I was always curious about programming as a teenager, but I never really tried to code,
-                just because it seemed very hard for me. That was until I decided to try with JavaScript and signed for the
+                I have always been curious about programming since I was a teenager, but I never really tried coding because it seemed difficult to me. That changed when I decided to give JavaScript a try and enrolled in the
                 <motion.a layout href=' https://learn.telerikacademy.com/mod/customcert/verify_certificate.php?contextid=99501&code=1ESFKS8jqp&qrcode=1&fbclid=IwZXh0bgNhZW0CMTEAAR1Scg5ngvph6NZMHWb4_vpqrpS9BAopaMa5c7abiqPHD6f6r_UwS6Lu2WQ_aem_3juBr2OfN-SxyLSxJCx1fw' target='_blank'> 6-month Alpha JavaScript Track Program</motion.a>.
-                With the passing of the entrance exam my desire for coding just got bigger and after 6 months, a lot of tasks solved and 4 simple projects behind my back 
-                I am looking for opportunities to solidify my skills and learn new ones.
+                After passing the entrance exam, my passion for coding grew, and after six months of hard work and completing four simple projects, I am now seeking opportunities to solidify my skills and learn new ones.
+                While learning web development, I am also studying Cybersecurity at the Technical University of Varna (Bulgaria). During my first year, I went trough the basics of C++ such as OOP and DSA  and got to build some programs.
                 </motion.p>
             </motion.div>
             <Contact/> 
